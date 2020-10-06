f3dc5ea122508b94492ebf2a3d10cd67c7d9716c
Chad Banks <chadrbanks@yahoo.com>
2020-10-06 19:13:18 -0400

    MySQL Integration example.

---
+++ ch9/index.php
@@ -0,0 +1,48 @@
+<?php
+
+include( '../../../creds.php' );
+
+$host = '127.0.0.1';
+$db   = 'crbanks1';
+$user = 'crbanks1';
+$pass = MYSQLPASS;
+$charset = 'utf8';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$opt =
+	[
+		PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+		PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+		PDO::ATTR_EMULATE_PREPARES => false,
+	];
+
+$pdo = new PDO($dsn, $user, $pass, $opt);
+
+var_dump( $pdo );
+
+?><hr /><?php
+
+$qry = 'SHOW TABLES;';
+
+$r = $pdo->query( $qry );
+
+while ($row = $r->fetch())
+{
+	echo '<br><br>' . $row["Tables_in_crbanks1"];
+}
+
+echo '<hr />';
+
+
+$qry = 'SELECT COUNT(*) FROM `users`; ';
+
+$r = $pdo->query( $qry );
+
+while ($row = $r->fetch())
+{
+	echo '<br><br>This table has ' . $row["COUNT(*)"] . ' rows of data.';
+}
+
+
+
+
